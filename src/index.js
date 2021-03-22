331543e9360253f9865eb2ec6245958c82458d74
christeen24 <christeen@incubatelabs.com>
2021-03-22 22:41:07 +0530

    main ui structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/css/styles.css";
+
+//components
+import JobPostings from "./pages/JobPosting.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/jobs" component={JobPostings}></Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/JobPosting.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export default function JobPosting() {
+  return (
+    <div class="container mt-5 pt-5">
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-lg-8 mb-5">
+          <button
+            type="button"
+            class="btn btn-dark btn-lg btn-block shuffle-btn"
+          >
+            Shuffle jobs
+          </button>
+        </div>
+      </div>
+      <div class="row d-flex justify-content-center">
+        <div class="col-12 col-lg-8">
+          <div class="card mb-3 p-2">
+            <div class="d-flex flex-row">
+              <div class="col-1">logo</div>
+              <div class="col-11">
+                <div class="d-flex justify-content-between">
+                  <h6 class="job-title">Product Manager</h6>
+                  <h6 class="created-date">
+                    <i class="fa fa-clock"></i> &nbsp; 2 days ago
+                  </h6>
+                </div>
+                <h6 class="company-name">Paladin Analytics</h6>
+                <h6 class="category">
+                  Information & Communication Technology &gt; Product Management
+                  & Development
+                </h6>
+                <span class="job-type">
+                  <i class="fa fa-briefcase"></i> &nbsp;Full-time
+                </span>{" "}
+                &nbsp;&nbsp;
+                <span class="location">
+                  <i class="fa fa-globe"></i> &nbsp;Colombo, Sri lanka
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-8">
+          <div class="card mb-3 p-2">
+            <div class="d-flex flex-row">
+              <div class="col-1">logo</div>
+              <div class="col-11">
+                <div class="d-flex justify-content-between">
+                  <h6 class="job-title">Product Manager</h6>
+                  <h6 class="created-date">
+                    <i class="fa fa-clock"></i> &nbsp; 2 days ago
+                  </h6>
+                </div>
+                <h6 class="company-name">Paladin Analytics</h6>
+                <h6 class="category">
+                  Information & Communication Technology &gt; Product Management
+                  & Development
+                </h6>
+                <span class="job-type">
+                  <i class="fa fa-briefcase"></i> &nbsp;Full-time
+                </span>{" "}
+                &nbsp;&nbsp;
+                <span class="location">
+                  <i class="fa fa-globe"></i> &nbsp;Colombo, Sri lanka
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-8">
+          <div class="card mb-3 p-2">
+            <div class="d-flex flex-row">
+              <div class="col-1">logo</div>
+              <div class="col-11">
+                <div class="d-flex justify-content-between">
+                  <h6 class="job-title">Product Manager</h6>
+                  <h6 class="created-date">
+                    <i class="fa fa-clock"></i> &nbsp; 2 days ago
+                  </h6>
+                </div>
+                <h6 class="company-name">Paladin Analytics</h6>
+                <h6 class="category">
+                  Information & Communication Technology &gt; Product Management
+                  & Development
+                </h6>
+                <span class="job-type">
+                  <i class="fa fa-briefcase"></i> &nbsp;Full-time
+                </span>{" "}
+                &nbsp;&nbsp;
+                <span class="location">
+                  <i class="fa fa-globe"></i> &nbsp;Colombo, Sri lanka
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-8">
+          <div class="card mb-3 p-2">
+            <div class="d-flex flex-row">
+              <div class="col-1">logo</div>
+              <div class="col-11">
+                <div class="d-flex justify-content-between">
+                  <h6 class="job-title">Product Manager</h6>
+                  <h6 class="created-date">
+                    <i class="fa fa-clock"></i> &nbsp; 2 days ago
+                  </h6>
+                </div>
+                <h6 class="company-name">Paladin Analytics</h6>
+                <h6 class="category">
+                  Information & Communication Technology &gt; Product Management
+                  & Development
+                </h6>
+                <span class="job-type">
+                  <i class="fa fa-briefcase"></i> &nbsp;Full-time
+                </span>{" "}
+                &nbsp;&nbsp;
+                <span class="location">
+                  <i class="fa fa-globe"></i> &nbsp;Colombo, Sri lanka
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
